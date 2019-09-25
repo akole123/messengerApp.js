e9f1c350c3851b503b7a33666819ca87ae730264
akole123 <akulzhen123@gmail.com>
2019-09-24 22:01:54 -0700

    Rest of the files

---
+++ social-media/frontend/src/App.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './components/pages/Home';
+import Users from './components/pages/Users';
+import Profile from './components/pages/Profile';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserListState from './components/userlist/UserListState';
+import AuthState from './components/authlist/AuthState';
+import ChatState from './components/chatlist/ChatState';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatState>
+            <AuthState>
+                <UserListState>
+                    <Router>
+                        <Fragment>
+                            <Navbar />
+                            <div className="container">
+                                <Switch>
+                                    <Route exact path='/' component={Home} />
+                                    <Route exact path='/profile' component={Profile} />
+                                    <Route exact path='/users' component={Users} />
+                                </Switch>
+                            </div>
+                            <Footer />
+                        </Fragment>
+                    </Router>
+                </UserListState >
+            </AuthState>
+        </ChatState>
+    );
+}
+
+export default App;
